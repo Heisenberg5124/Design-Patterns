9311df94c85cd81cc37d96f412043a753eaef308
Đặng Hùng Vĩnh <48090305+Heisenberg5124@users.noreply.github.com>
2021-05-07 21:06:51 +0700

    Observer Pattern Implementation

---
+++ src/observer/subject/CurrentConditionsDisplay.java
@@ -0,0 +1,28 @@
+package observer.subject;
+
+import observer.observer.DisplayElement;
+import observer.observer.Observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+
+    private float temperature;
+    private float humidity;
+    private Subject weatherData;
+
+    public CurrentConditionsDisplay(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Current conditions: %fF degrees and %f humidity\n", temperature, humidity);
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        display();
+    }
+}
